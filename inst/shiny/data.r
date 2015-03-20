238b1216cb45a19ab13cc2147122a62025c3c895
wrathematics <wrathematics@gmail.com>
2015-03-20 19:24:50 -0400

    added skeleton data tab

---
+++ inst/shiny/data.r
@@ -0,0 +1,81 @@
+### 'Data' tab
+
+output$main_data <- renderUI({
+  list(
+      mainPanel(id="datatabs", 
+        tabsetPanel(
+#          tabPanel("Manage", htmlOutput("data_manage")),
+          tabPanel("Transform", htmlOutput("data_transform"))
+        )
+      )
+    )
+})
+
+output$tabs_data <- renderUI({
+  changed <- buttonfix(session, input$button_process)
+  
+  if (changed$button_process)
+    "asfd"
+  else
+    "qwer"
+})
+
+
+### Transform
+
+output$data_transform <- renderUI({
+  list(
+    sidebarLayout(
+      sidebarPanel(
+        checkboxInput("data_transform_checkbox_makelower", "Make lowercase?", value=FALSE),
+        checkboxInput("data_transform_checkbox_rempunct", "Remove punctuation?", value=FALSE),
+        checkboxInput("data_transform_checkbox_remnum", "Remove numbers?", value=FALSE),
+        checkboxInput("data_transform_checkbox_remws", "Remove extra whitespace?", value=FALSE),
+        checkboxInput("data_transform_checkbox_stem", "Stem?", value=FALSE),
+        selectizeInput("data_stopwords_lang", "Stopwords Language", stopwords_list, "english"),
+        checkboxInput("data_transform_checkbox_remstop", "Remove stopwords?", value=FALSE),
+        actionButton("button_process", "Transform")
+      ),
+    mainPanel(
+      htmlOutput("data_transform_buttonaction")
+      )
+    )
+  )
+})
+
+output$data_transform_buttonaction <- renderUI({
+  button <- buttonfix(session, input$button_process)
+  
+  if (button$button_process)
+  {
+    withProgress(message='Processing...', value=0,{
+      corpus <- get("corpus", envir=session)
+      
+      if (input$data_transform_checkbox_makelower)
+        corpus <- tm::tm_map(corpus, tm::content_transformer(tolower))
+      if (input$data_transform_checkbox_rempunct)
+        corpus <- tm::tm_map(corpus, tm::removePunctuation)
+      if (input$data_transform_checkbox_remnum)
+        corpus <- tm::tm_map(corpus, tm::removeNumbers)
+      if (input$data_transform_checkbox_remws)
+        corpus <- tm::tm_map(corpus, tm::stripWhitespace)
+      if (input$data_transform_checkbox_stem)
+        corpus <- tm::tm_map(corpus, tm::stemDocument)
+      if (input$data_transform_checkbox_remstop)
+        corpus <- tm::tm_map(corpus, tm::removeWords, tm::stopwords(input$data_stopwords_lang))
+      
+      assign("corpus", corpus, envir=session)
+      tdm <- tm::TermDocumentMatrix(corpus)
+      assign("tdm", tdm, envir=session)
+      
+      wordcount_table <- sort(rowSums(as.matrix(tdm)), decreasing=TRUE)
+      assign("wordcount_table", wordcount_table, envir=session)
+      
+      
+      "Done processing!"
+    })
+  }
+  else
+    "Do something"
+})
+
