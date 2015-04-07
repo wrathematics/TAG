cad6942f98ccbd10390aebc2bfa2b90b15260ebd
wrathematics <wrathematics@gmail.com>
2015-04-07 19:25:58 -0400

    updated sourceror

--- inst/shiny/sourcerer.r
+++ inst/shiny/sourcerer.r
@@ -1,6 +1,7 @@
 source(file="pages/about.r", local=TRUE)
 
 source(file="pages/data.r", local=TRUE)
+source(file="pages/data/manage.r", local=TRUE)
 source(file="pages/data/transform.r", local=TRUE)
 
 source(file="pages/summarize.r", local=TRUE)
