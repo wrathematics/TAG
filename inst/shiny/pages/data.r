33b44ccc86ad8179e0c95de1590b4ea1e87d6e6f
wrathematics <wrathematics@gmail.com>
2015-04-07 19:25:39 -0400

    added 'manage' template

--- inst/shiny/pages/data.r
+++ inst/shiny/pages/data.r
@@ -2,7 +2,7 @@ output$main_data <- renderUI({
   list(
       mainPanel(id="datatabs", 
         tabsetPanel(
-#          tabPanel("Manage", htmlOutput("data_manage")),
+          tabPanel("Manage", htmlOutput("data_manage")),
           tabPanel("Transform", htmlOutput("data_transform"))
         )
       )
---
+++ inst/shiny/pages/data/manage.r
@@ -0,0 +1,18 @@
+output$data_manage <- renderUI({
+  list(
+    sidebarLayout(
+      sidebarPanel(
+        radioButtons(inputId="data_infile", label="Select File:", c("txt"="txt", "dir"="dir", "rda"="rda"), selected="txt", inline=TRUE),
+        conditionalPanel(condition = "input.data_infile == 'state'",
+          fileInput('uploadState', 'Load previous app state:', accept=".rda"),
+          uiOutput("refreshOnUpload")
+        )
+      )#,
+#    mainPanel(
+#      htmlOutput("data_transform_buttonaction")
+#      )
+    )
+  )
+})
+
+
