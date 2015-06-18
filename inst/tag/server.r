d468dfb8430c0145352b33fc3a087ee6207b0ab5
wrathematics <wrathematics@gmail.com>
2015-06-18 13:26:47 -0400

    fixed sourcing

--- inst/tag/server.r
+++ inst/tag/server.r
@@ -8,22 +8,28 @@ source(file="shiny/utils/help.r")
 
 stopwords_list <- c("danish", "dutch", "english", "finnish", "french", "german", "hungarian", "italian", "norwegian", "portuguese", "russian", "spanish", "swedish")
 
+booklist_names <- readLines("data/books/booklist_names.txt")
+booklist <- dir("data/books/", pattern=".rda")
 
 # manage, view, visualize, explore, transform
 shinyServer(
   function(input, output, session)
   {
     ### Data preprocessing TODO
-    data("crude")
-    corpus <- crude
-    assign("corpus", corpus, envir=session)
+#    data("crude")
+#    corpus <- crude
+#    assign("corpus", corpus, envir=session)
+#    
+#    tdm <- tm::TermDocumentMatrix(corpus)
+#    assign("tdm", tdm, envir=session)
+#    
+#    wordcount_table <- sort(rowSums(as.matrix(tdm)), decreasing=TRUE)
+#    assign("wordcount_table", wordcount_table, envir=session)
     
-    tdm <- tm::TermDocumentMatrix(corpus)
-    assign("tdm", tdm, envir=session)
     
-    wordcount_table <- sort(rowSums(as.matrix(tdm)), decreasing=TRUE)
-    assign("wordcount_table", wordcount_table, envir=session)
-    
-    source(file="shiny/sourcerer.r", local=TRUE)
+    files <- dir("./shiny", recursive=TRUE, pattern="[.]r$")
+    files <- paste0("./shiny/", files)
+    print(files)
+    for (file in files) source(file=file, local=TRUE)
   }
 )
