da9ffdef0efa459d4e9c0fd19c61f1b2fd729677
wrathematics <wrathematics@gmail.com>
2015-06-19 15:45:14 -0400

    added xsede logo

--- inst/tag/ui.r
+++ inst/tag/ui.r
@@ -2,19 +2,24 @@ library(shiny)
 
 
 shinyUI(
-  navbarPage("TAG", windowTitle="TAG: Text Analytics Gateway", id="nav_tag", 
-    inverse=TRUE, collapsible=FALSE, 
-#    theme="superhero.css", 
-    
-    
-    tabPanel("About", uiOutput('main_about')),
-    tabPanel("Data", uiOutput('main_data')),
-    navbarMenu("Summarize",
-      tabPanel("Basic", uiOutput("main_summarize_basic")),
-      tabPanel("Plots", uiOutput("main_summarize_plot"))
+  fluidPage(
+    tags$footer(
+      tags$link(rel="stylesheet", type="text/css", href="css/logo.css")
     ),
-    navbarMenu("Analyse",
-      tabPanel("LDA", uiOutput("main_analyse_lda"))
+    
+    navbarPage(title=div(a(href="http://xsede.org", target="new", img(src="img/xsede_small.png"))), 
+      windowTitle="TAG: Text Analytics Gateway", id="nav_tag", 
+      inverse=TRUE, collapsible=FALSE, 
+      
+      tabPanel("About", uiOutput('main_about')),
+      tabPanel("Data", uiOutput('main_data')),
+      navbarMenu("Summarize",
+        tabPanel("Basic", uiOutput("main_summarize_basic")),
+        tabPanel("Plots", uiOutput("main_summarize_plot"))
+      ),
+      navbarMenu("Analyse",
+        tabPanel("LDA", uiOutput("main_analyse_lda"))
+      )
     )
   )
 )
