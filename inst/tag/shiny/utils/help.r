1d6ae4c0d32602b24551da3beecdd04966424ea9
wrathematics <wrathematics@gmail.com>
2015-06-23 13:35:22 -0400

    add help renderers

--- inst/tag/shiny/utils/help.r
+++ inst/tag/shiny/utils/help.r
@@ -34,3 +34,66 @@ render_helpfile <- function(title, file)
   HTML(html)
 }
 
+
+
+
+# This is way more genearl than we need now; originally the help
+# menu's radio buttons were checkboxes, but it was kind of dumb soooo
+render_helpfile_inplace <- function(title, files)
+{
+  files <- paste0("shiny/help/", files)
+  
+  raw <- paste("#", title_case(title), "\n\n")
+  for (file in files)
+  {
+    raw <- c(raw, "", readLines(file))
+  }
+  
+  body <- markdown::markdownToHTML(text=raw, fragment.only=TRUE, options=c(""))
+  
+  link <- paste0(gsub(title, pattern=" ", replacement=""), "_help")
+  thisyear <- format(Sys.Date(), "%Y")
+  
+  html <- sprintf("
+    %s
+    <br>
+    <font size='1'>
+      &copy;%s Drew Schmidt and Mike Black.
+      All documentation is released under a
+      <a rel='license' href='http://creativecommons.org/licenses/by-sa/4.0/' target='_blank'>Creative Commons License</a>.
+      <img alt='' style='border-width:0' src='cc.png'>
+    </font>
+     ", body, thisyear, link)
+  
+  html <- enc2utf8(html)
+  HTML(html)
+}
+
+
+
+from_md_to_display <- function(terms)
+{
+  terms <- gsub(terms, pattern="_", replacement=" ")
+  terms <- gsub(terms, pattern=".md", replacement="")
+  terms <- title_case(terms)
+  
+  ### special cases that have to be dealt with :(
+  terms <- gsub(terms, pattern="Lda ", replacement="LDA ")
+  
+  terms
+}
+
+
+
+from_display_to_md <- function(terms)
+{
+  terms <- tolower(terms)
+  terms <- gsub(terms, pattern=" ",  replacement="_")
+  paste0(terms, ".md")
+}
+
+
+
+
+
+
