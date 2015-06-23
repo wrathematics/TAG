99d76f9b7305bbfc4d37dd2f5ccedbd33e26b8e3
wrathematics <wrathematics@gmail.com>
2015-06-23 15:22:44 -0400

    globals explanation, helpdirs_display

--- inst/tag/global.r
+++ inst/tag/global.r
@@ -1,12 +1,19 @@
 source(file="shiny/utils/help.r")
 source(file="shiny/utils/validate.r")
 
+
+### Filter globals
 stopwords_list <- c("danish", "dutch", "english", "finnish", "french", "german", "hungarian", "italian", "norwegian", "portuguese", "russian", "spanish", "swedish")
 
+
+### Data import---book globals
 booklist_names <- readLines("data/books/booklist_names.txt")
 booklist <- dir("data/books/", pattern=".rda")
 
 
+### Help system globals
 helpdirs <- dir("shiny/help")
 helppages <- lapply(helpdirs, function(page) dir(paste0("shiny/help/", page), pattern="[.]md$"))
 names(helppages) <- helpdirs
+helpdirs_display <- c("", helpdirs)
+names(helpdirs_display) <- c("", from_md_to_display(helpdirs))
