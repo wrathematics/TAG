7b59c375dbac33bbf76c07c503ff0f805520624d
wrathematics <wrathematics@gmail.com>
2015-06-23 13:35:54 -0400

    sketch of reset button for data

--- inst/tag/shiny/pages/data/input.r
+++ inst/tag/shiny/pages/data/input.r
@@ -8,7 +8,8 @@ output$data_import <- renderUI({
         uiOutput("refreshOnUpload")
       ),
       selectizeInput("data_books", "Books", booklist_names),
-      actionButton("button_data_input_book", "Choose!")
+      actionButton("button_data_input_book", "Choose!"),
+      actionButton("button_data_input_clear", "Clear")
     ),
     mainPanel(
       htmlOutput("data_input_book_buttonaction")
@@ -19,32 +20,46 @@ output$data_import <- renderUI({
 
 
 output$data_input_book_buttonaction <- renderUI({
-  temp <- eventReactive(input$button_data_input_book, {
-    withProgress(message='Loading data...', value=0, {
-      runtime <- system.time({
-        book <- input$data_books
-        bookfile <- booklist[which(booklist_names == book)]
+  temp <- eventReactive(ignoreNULL=FALSE, input$button_data_input_book, {
+    if (input$button_data_input_book > 0)
+    {
+      withProgress(message='Loading data...', value=0, {
+        runtime <- system.time({
+          book <- input$data_books
+          bookfile <- booklist[which(booklist_names == book)]
+          
+          load(paste0("data/books/", bookfile))
+          
+          localstate$corpus <- corpus
+          localstate$tdm <- tdm
+          localstate$wordcount_table <- wordcount_table
+          
+  #        assign("corpus", corpus, envir=session)
+  #        tdm <- tm::TermDocumentMatrix(corpus)
+  #        assign("tdm", tdm, envir=session)
+  #        wordcount_table <- sort(rowSums(as.matrix(tdm)), decreasing=TRUE)
+  #        assign("wordcount_table", wordcount_table, envir=session)
+        })
         
-        load(paste0("data/books/", bookfile))
+        localstate$lda_mdl <- NULL
         
-        localstate$corpus <- corpus
-        localstate$tdm <- tdm
-        localstate$wordcount_table <- wordcount_table
-        
-#        assign("corpus", corpus, envir=session)
-#        tdm <- tm::TermDocumentMatrix(corpus)
-#        assign("tdm", tdm, envir=session)
-#        wordcount_table <- sort(rowSums(as.matrix(tdm)), decreasing=TRUE)
-#        assign("wordcount_table", wordcount_table, envir=session)
+        setProgress(1)
       })
       
-      setProgress(1)
-    })
-    
-    HTML(paste("The<i>", input$data_books, "</i>corpus is now ready to use!\nLoading finished in", round(runtime[3], roundlen), "seconds."))
+      HTML(paste("The<i>", input$data_books, "</i>corpus is now ready to use!\nLoading finished in", round(runtime[3], roundlen), "seconds."))
+    }
   })
   
-  temp()
+  observeEvent(input$button_data_input_clear, {
+    localstate$corpus <- NULL
+    localstate$tdm <- NULL
+    localstate$wordcount_table <- NULL
+    localstate$lda_mdl <- NULL
+    
+    HTML("Cleared!")
+  })
   
+  out <- temp()
+  if (is.null(localstate$corpus)) return() else out
 })
 
