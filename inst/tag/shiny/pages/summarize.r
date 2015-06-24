6625cafed3b0ea2d8880f1f9dfaaf6e89f7248a2
wrathematics <wrathematics@gmail.com>
2015-06-24 19:05:22 -0400

    added dispersion plot

--- inst/tag/shiny/pages/summarize.r
+++ inst/tag/shiny/pages/summarize.r
@@ -14,6 +14,7 @@ output$main_summarize_plot <- renderUI({
     tabsetPanel(
       tabPanel("Top 10", uiOutput("summarize_top10")),
       tabPanel("Correlation", uiOutput("summarize_wordcorr")),
+      tabPanel("Dispersion", uiOutput("summarize_dispersionplot")),
       tabPanel("Zipf Plot", uiOutput("summarize_zipf")),
       tabPanel("Wordcloud", uiOutput("summarize_wordcloud"))
     )
--- inst/tag/shiny/pages/summarize/plot.r
+++ inst/tag/shiny/pages/summarize/plot.r
@@ -139,3 +139,42 @@ output$summarize_wordcloud_plotter <- renderPlot({
   })
 })
 
+
+
+#test <- function() 
+
+output$summarize_dispersionplot <- renderUI(
+  sidebarLayout(
+    sidebarPanel(
+      h5("Dispersion Plot"),
+      checkboxInput("plot_dispersion_checkbox_findclosest", "Find closest match?", value=FALSE),
+      textInput("dispersionplot_word", "Word(s) (comma separated)", ""),
+      render_helpfile("Wordcloud", "summarize/plot_wordcloud.md")
+    ),
+    mainPanel(
+      renderPlot({
+        must_have("corpus")
+        
+        term <- input$dispersionplot_word
+        if (term == "")
+          return("")
+        
+        withProgress(message='Validating inputs...', value=0,
+        {
+          terms <- unlist(strsplit(term, split=","))
+          names <- names(localstate$wordcount_table)
+          nb.terms <- sapply(terms, function(term) find_closest_word(term, names)$word)
+          
+          if (input$plot_dispersion_checkbox_findclosest)
+            terms <- nb.terms
+          else if (!all(terms == nb.terms))
+            stop("asdf")
+          
+          setProgress(1/2, message="Rendering plot...")
+          qdap::dispersion_plot(localstate$corpus, terms, color="black", bg.color="white")
+        })
+      })
+    )
+  )
+)
+
