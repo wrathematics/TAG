9c914d1e0f47ed4f35ed3537f454a878ba8af108
wrathematics <wrathematics@gmail.com>
2015-07-01 14:44:14 -0400

    added TAG versioning helpers

---
+++ R/version.r
@@ -0,0 +1,11 @@
+#' @export
+get.tagversion  <- function() packageVersion("TAG")
+
+
+#' @export
+check.tagversion <- function(version)
+{
+  ### Define breakages in state, if they ever are created.
+  
+  TRUE
+}
