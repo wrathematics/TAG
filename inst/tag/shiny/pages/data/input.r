05f86e62e5c18e86c12f44f140b52374b402e685
Drew Schmidt <schmidt@math.utk.edu>
2015-07-13 20:55:11 -0400

    added file and textbox inputs

--- inst/tag/shiny/pages/data/input.r
+++ inst/tag/shiny/pages/data/input.r
@@ -24,19 +24,39 @@ update_wordcount <- function()
 output$data_import <- renderUI({
   sidebarLayout(
     sidebarPanel(
-      ### TODO
-#      radioButtons(inputId="data_infile", label="Select File:", c("txt"="txt", "dir"="dir", "rda"="rda"), selected="txt", inline=TRUE),
-      conditionalPanel(condition = "input.data_infile == 'state'",
-        fileInput('uploadState', 'Load previous app state:', accept=".rda"),
-        uiOutput("refreshOnUpload")
+      radioButtons(inputId="data_input_type", 
+                   label="Select Input Type", 
+                   c("Local File"="files", "Text Box"="box", "Book"="book", "Speech"="speech"), 
+                   selected="", inline=FALSE),
+      
+      # Local file
+      conditionalPanel(condition = "input.data_input_type == 'files'",
+        br(),
+        fileInput('data_localtext_file', label="Input File", 
+        multiple=FALSE, ### FIXME
+        accept=c(".txt"))
       ),
-      selectizeInput("data_books", "Books", extradata_books_titles),
-      actionButton("button_data_input_books", "Load Book"),
       
-      br(),br(),
+      # Text box
+      conditionalPanel(condition = "input.data_input_type == 'box'",
+        br(),
+        tags$textarea(id="data_input_textbox", rows=6, cols=40, ""),
+        actionButton("button_data_input_textbox", "Load Textbox")
+      ),
       
-      selectizeInput("data_speeches", "Speeches", extradata_speeches_titles),
-      actionButton("button_data_input_speeches", "Load Speech")
+      # Book
+      conditionalPanel(condition = "input.data_input_type == 'book'",
+        br(),
+        selectizeInput("data_books", "Books", extradata_books_titles),
+        actionButton("button_data_input_books", "Load Book")
+      ),
+      
+      # Speech
+      conditionalPanel(condition = "input.data_input_type == 'speech'",
+        br(),
+        selectizeInput("data_speeches", "Speeches", extradata_speeches_titles),
+        actionButton("button_data_input_speeches", "Load Speech")
+      )
     ),
     mainPanel(
       renderUI(localstate$input_out)
@@ -46,8 +66,72 @@ output$data_import <- renderUI({
 
 
 
+
 set_data <- function(input)
 {
+  # Local file
+  tmp <- eventReactive(input$data_localtext_file, {
+    textfile <- input$data_localtext_file
+    
+    if (!is.null(textfile))
+    {
+      clear_state()
+      
+      withProgress(message='Reading data...', value=0, {
+        runtime <- system.time({
+          dir <- sub(textfile$datapath, pattern="/[^/]*$", replacement="")
+          
+          setProgress(1/4, message="Creating corpus...")
+          localstate$corpus <- tm::Corpus(tm::DirSource(dir))
+          
+          setProgress(1/2, message="Creating tdm...")
+          localstate$tdm <- tm::TermDocumentMatrix(localstate$corpus)
+          
+          setProgress(3/4, message="Creating wordcounts...")
+          localstate$wordcount_table <- sort(rowSums(as.matrix(localstate$tdm)), decreasing=TRUE)
+        })
+      })
+      
+      localstate$input_out <- HTML(paste("Successfully loaded and processed", length(dir(dir)), "file(s) in", round(runtime[3], roundlen), "seconds."))
+    }
+    else
+      localstate$input_out <- HTML("")
+  })
+  
+  observe(tmp())
+  
+  
+  
+  # Text box
+  observeEvent(input$button_data_input_textbox, {
+    if (input$button_data_input_textbox > 0)
+    {
+      clear_state()
+      
+      withProgress(message='Reading data...', value=0, {
+        runtime <- system.time({
+          text <- unlist(strsplit(input$data_input_textbox, split="\n"))
+          
+          setProgress(1/4, message="Creating corpus...")
+          localstate$corpus <- tm::Corpus(tm::VectorSource(text))
+          
+          setProgress(1/2, message="Creating tdm...")
+          localstate$tdm <- tm::TermDocumentMatrix(localstate$corpus)
+          
+          setProgress(3/4, message="Creating wordcounts...")
+          localstate$wordcount_table <- sort(rowSums(as.matrix(localstate$tdm)), decreasing=TRUE)
+        })
+        
+        setProgress(1)
+      })
+      
+      localstate$input_out <- HTML(paste("Your text box corpus is now ready to use!\nLoading and processing finished in", round(runtime[3], roundlen), "seconds."))
+    }
+  })
+  
+  
+  
+  # Book
   observeEvent(input$button_data_input_books, {
     if (input$button_data_input_books > 0)
     {
@@ -73,6 +157,8 @@ set_data <- function(input)
   })
   
   
+  
+  # Speech
   observeEvent(input$button_data_input_speeches, {
     if (input$button_data_input_speeches > 0)
     {
