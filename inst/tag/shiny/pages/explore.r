72154e7163121c9f52963628380eb576bacbfc4b
Drew Schmidt <schmidt@math.utk.edu>
2015-07-13 20:54:48 -0400

    vastly improved termsearch in explore tab

--- inst/tag/shiny/pages/explore.r
+++ inst/tag/shiny/pages/explore.r
@@ -88,29 +88,53 @@ output$explore_termsearch <- renderUI({
   list(
     sidebarLayout(
       sidebarPanel(
-        h5("Term Frequency"),
-        checkboxInput("basic_termsearch_checkbox_findclosest", "Find closest match?", value=FALSE),
-        textInput("explore_termsearchbox", ""),
+        radioButtons(inputId="explore_termsearch_type", 
+                   label="Search by...", c("Term"="Term", "Frequency"="Frequency"), 
+                   selected="", inline=FALSE),
+        
+        br(),
+        
+        conditionalPanel(condition = "input.explore_termsearch_type == 'Term'",
+          h5(strong("Search Strictness")),
+          checkboxInput("basic_termsearch_checkbox_findclosest", "Find closest match?", value=FALSE),
+          textInput("explore_termsearchbox", "")
+        ),
+        
+        conditionalPanel(condition = "input.explore_termsearch_type == 'Frequency'",
+          sliderInput("explore_termsearch_minwordfreq", "Minimum Frequency %", min=0, max=100, value=1),
+          sliderInput("explore_termsearch_maxwordfreq", "Maximum Frequency %", min=0, max=100, value=100)
+        ),
+        
         render_helpfile("Term Search", "explore/basic_termsearch.md")
       ),
       mainPanel(
         renderUI({
           must_have("wordcount_table")
           
-          if (input$explore_termsearchbox == "")
+          if (is.null(input$explore_termsearch_type))
             return("")
           
-          term <- input$explore_termsearchbox
-          
-          if (input$basic_termsearch_checkbox_findclosest)
-            term <- find_closest_word(term, names(localstate$wordcount_table))$word
-          
-          freq <- localstate$wordcount_table[term]
-          
-          if (is.na(freq))
-            HTML("Term not found! <br><br> You may need to transform the data first (stem, lowercase, etc.).  See the Data--Transform tab.")
-          else
-            paste0("\"", term, "\" occurs ", freq, " times in the corpus.")
+          if (input$explore_termsearch_type == "Term")
+          {
+            if (input$explore_termsearchbox == "")
+              return("")
+            
+            term <- input$explore_termsearchbox
+            
+            if (input$basic_termsearch_checkbox_findclosest)
+              term <- find_closest_word(term, names(localstate$wordcount_table))$word
+            
+            freq <- localstate$wordcount_table[term]
+            
+            if (is.na(freq))
+              HTML("Term not found! <br><br> You may need to transform the data first (stem, lowercase, etc.).  See the Data--Transform tab.")
+            else
+              paste0("\"", term, "\" occurs ", freq, " times in the corpus.")
+          }
+          else if (input$explore_termsearch_type == "Frequency")
+          {
+            DT::dataTableOutput("explore_termsearch_frequency_rendertable")
+          }
         })
       )
     )
@@ -118,6 +142,26 @@ output$explore_termsearch <- renderUI({
 })
 
 
+output$explore_termsearch_frequency_rendertable <- DT::renderDataTable({
+    if (input$explore_termsearch_minwordfreq > input$explore_termsearch_maxwordfreq)
+      stop("Bad inputs; must have max frequency >= min frequency")
+    
+    tab <- localstate$wordcount_table*100/sum(localstate$wordcount_table)
+            ind <- which(tab >= input$explore_termsearch_minwordfreq & tab <= input$explore_termsearch_maxwordfreq)
+    
+    df <- data.frame(Count=localstate$wordcount_table[ind], Frequency=tab[ind])
+    
+    DT::datatable(df, extensions="Scroller", escape=TRUE,
+      options = list(
+        searching = FALSE,
+        deferRender = TRUE,
+        dom = "frtiS",
+        scrollY = 300,
+        width = 500,
+        scrollCollapse = TRUE
+      )
+    )
+})
 
 
 output$explore_top10 <- renderUI(
