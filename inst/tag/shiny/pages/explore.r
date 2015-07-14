331ee43ac6f47bde17485ae6611cb5f33017cc63
wrathematics <wrathematics@gmail.com>
2015-07-14 13:19:29 -0400

    made basic exploration horizontal

--- inst/tag/shiny/pages/explore.r
+++ inst/tag/shiny/pages/explore.r
@@ -53,30 +53,33 @@ output$explore_corpus <- renderUI({
   
   df <- data.frame(Count=sapply(l, identity))
   
-  verticalLayout(
-    renderTable(df, align=c("l", "r")),
-    br(),
-    renderPlot({
-      must_have("corpus")
-      
-      df <- data.frame(length=1:length(localstate$explore_wordlens), characters=localstate$explore_wordlens)
-      indices <- df$characters > 0
-      df <- df[indices, ]
-      df$characters <- 100*df$characters/sum(df$characters)
-      
-      breaks <- df$length
-      labs <- as.character(breaks)
-      if (input$explore_corpus_maxwordlen == labs[length(labs)])
-        labs[length(labs)] <- paste0(tail(labs, 1), "+")
-      
-      ggplot(data=df, aes(length, characters)) + 
-        geom_bar(stat="identity") + 
-        scale_x_continuous(breaks=breaks, labels=labs) +
-        xlab("Characters") +
-        ylab("Percentage of Corpus") +
-        ggtitle("Distribution of Words by Character Length") + 
-        theme_bw()
-    })
+  fluidRow(
+    column(4, 
+      renderTable(df, align=c("l", "r"))
+    ),
+    column(8, 
+      renderPlot({
+        must_have("corpus")
+        
+        df <- data.frame(length=1:length(localstate$explore_wordlens), characters=localstate$explore_wordlens)
+        indices <- df$characters > 0
+        df <- df[indices, ]
+        df$characters <- 100*df$characters/sum(df$characters)
+        
+        breaks <- df$length
+        labs <- as.character(breaks)
+        if (input$explore_corpus_maxwordlen == labs[length(labs)])
+          labs[length(labs)] <- paste0(tail(labs, 1), "+")
+        
+        ggplot(data=df, aes(length, characters)) + 
+          geom_bar(stat="identity") + 
+          scale_x_continuous(breaks=breaks, labels=labs) +
+          xlab("Characters") +
+          ylab("Percentage of Corpus") +
+          ggtitle("Distribution of Words by Character Length") + 
+          theme_bw()
+      })
+    )
   )
 })
 
