3725a8e1a165f991313cd4b03b983efb6e44e5b9
ChadBurdyshaw <chadburdyshaw@hotmail.com>
2016-05-23 09:31:15 -0400

    filter.r modified to cast corpus into lower caseto conform to stopword list

--- inst/tag/shiny/pages/data/filter.r
+++ inst/tag/shiny/pages/data/filter.r
@@ -20,27 +20,26 @@ output$data_filter <- renderUI({
         # Custom
         conditionalPanel(condition = "input.data_filter_type == 'custom'",
           br(),
-          checkboxInput("data_filter_checkbox_greedy", "Exclude greedily?", value=FALSE),
-          checkboxInput("data_filter_checkbox_greedy", "Exclude ignores case?", value=FALSE),
+          #checkboxInput("data_filter_checkbox_greedy", "Exclude greedily?", value=FALSE),
+          #checkboxInput("data_filter_checkbox_greedy", "Exclude ignores case?", value=FALSE),
           textInput("data_filter_exclude", "Exclude Text"),
           actionButton("button_data_filter_custom", "Filter Custom"),
           hr()
         ),
         
-        
         render_helpfile("Data Filter", "data/filter.md")
       ),
+
       mainPanel(
         renderUI({
           must_have("corpus")
-          
           a <- data_filter_stopwords_reactive()
           b <- data_filter_custom_reactive()
-          
-          if (!is.null(a)) a
-          else
-            b
+          if (!is.null(a)){ a() }
+          else{ b() }
+          localstate$input_out 
         })
+
       )
     )
   )
@@ -50,61 +49,64 @@ output$data_filter <- renderUI({
 
 data_filter_stopwords_reactive <- eventReactive(input$button_data_filter_stopwords, {
   withProgress(message='Processing...', value=0, {
-    
     addto_call("### Filter stopwords\n")
-    
     runtime <- system.time({
-      incProgress(0, message="Removing stopwords...")
-      evalfun(localstate$corpus <- tm::tm_map(localstate$corpus, tm::removeWords, tm::stopwords(input$data_filter_stopwords_lang)), 
-        comment="Remove stopwords")
-      
+      incProgress(0, message="Converting to Lower Case...")
+      evalfun(localstate$corpus <- tm::tm_map(localstate$corpus, tm::content_transformer(tolower)), 
+              comment="Transform to Lower Case")
+      incProgress(1/2, message="Removing Stopwords...")
+      evalfun(localstate$corpus <- tm::tm_map(localstate$corpus, tm::removeWords, 
+                                              tm::stopwords(input$data_filter_stopwords_lang)), 
+              comment="Remove Stopwords")
       clear_secondary()
-      
       addto_call("\n")
-      
       clear_modelstate()
     })
-    
     setProgress(1)
   })
-  
   paste("Processing finished in", round(runtime[3], roundlen), "seconds.")
+
+  localstate$input_out <- HTML("")
 })
-
-
+observe(data_filter_stopwords_reactive())
 
 
 data_filter_custom_reactive <- eventReactive(input$button_data_filter_custom, {
-  withProgress(message='Processing...', value=0, {
-    
-    addto_call("### Filter custom list\n")
-    
-    runtime <- system.time({
-      
-      if (input$data_filter_exclude != "")
-      {
+  if (input$data_filter_exclude != "")
+  {
+    withProgress(message='Processing...', value=0, {
+      addto_call("### Filter custom list\n")
+      runtime <- system.time({
+        incProgress(0, message="Converting to Lower Case...")
+        evalfun(localstate$corpus <- tm::tm_map(localstate$corpus, tm::content_transformer(tolower)), 
+              comment="Transform to Lower Case")
+        incProgress(0, message="Removing Custom Stopwords...")
         evalfun({
           terms <- input$data_filter_exclude
+          #convert exclusion words to lower case
+          terms <- tolower(terms) 
           terms <- unlist(strsplit(terms, split=","))
-          
           localstate$corpus <- tm::tm_map(localstate$corpus, tm::removeWords, terms)
-        })
-      }
-      else
-        stop("Exclusion list is empty!")
-      
-      cleare_secondary()
-      
-      addto_call("\n")
-      
-      clear_modelstate()
+        },comment="Remove Custom Stopwords")
+        clear_secondary()
+        addto_call("\n")
+        clear_modelstate()
+        setProgress(1)
+      })
+      paste("Processing finished in", round(runtime[3], roundlen), "seconds.")
     })
-    
-    setProgress(1)
-  })
-  
-  paste("Processing finished in", round(runtime[3], roundlen), "seconds.")
+  localstate$input_out <- HTML("")
+  }
+  else
+  {
+    #stop("Exclusion list is empty!")
+    localstate$input_out <- HTML("Exclusion list is empty!")
+  }
 })
+observe(data_filter_custom_reactive())
+
+
+
 
 ### TODO
 #        terms <- input$data_filter_exclude
