b248fafc80ed6caefc4b3bfef676bbe6d39badb1
wrathematics <wrathematics@gmail.com>
2017-06-08 13:11:41 -0400

    fix docs

--- R/version.r
+++ R/version.r
@@ -1,17 +1,27 @@
+#' TAG Versioning
+#' 
+#' Utilities used by the TAG system to ensure proper versioning across saved
+#' state files.
+#' 
+#' @name versioning
+#' @rdname versioning
+NULL
+
+
+
+#' @rdname versioning
 #' @export
 get.tagversion  <- function() packageVersion("TAG")
 
 
 
-### Define breakages in state, if they ever are created.
-get.oldestsupported <- function() as.package_version("0.0.7")
-
-
-
+#' @param version A package version.
+#' @rdname versioning
 #' @export
 check.tagversion <- function(version)
 {
-  if (version < get.oldestsupported())
+  OLDEST_SUPPORTED = as.package_version("0.0.7")
+  if (version < OLDEST_SUPPORTED)
     stop(paste0("The version this TAG statefile was created under (", version, ") is no longer supported.  You will need to recreate your analysis."))
   
   invisible(TRUE)
