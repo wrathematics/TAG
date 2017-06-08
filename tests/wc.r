bc1cd34bc5b720455f103f60db7a0faaaae6d7f9
wrathematics <wrathematics@gmail.com>
2017-06-08 12:53:34 -0400

    fix wc test

--- tests/wc.r
+++ tests/wc.r
@@ -4,40 +4,52 @@ text <- c("a b a c a b b", "1 2 3
      4   5        6 ", "1 2 3 4")
 
 test <- wc(text)
-truth <- structure(list(chars = 46L, letters = 7L, whitespace = 28L, punctuation = 0L, 
-    digits = 10L, words = 17L, sentences = 0L, lines = 4L), .Names = c("chars", 
-"letters", "whitespace", "punctuation", "digits", "words", "sentences", 
-"lines"))
+truth <-
+structure(list(chars = 46L, letters = 7L, whitespace = 28L, punctuation = 0L, 
+    digits = 10L, words = 17L, sentences = 0L, lines = 4L, wordlens = c(17L, 
+    0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L
+    ), senlens = c(0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 
+    0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L), syllens = c(3L, 0L, 
+    0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L)), .Names = c("chars", "letters", 
+"whitespace", "punctuation", "digits", "words", "sentences", 
+"lines", "wordlens", "senlens", "syllens"))
 
-stopifnot(all(sapply(2:length(test)-1, function(i) test[[i]] == truth[[i]])))
+stopifnot(identical(test, truth))
 
 
 
 
 
-load("../inst/tag/data/books/alice.rda")
+load("../inst/extradata/books/alice.rda")
 buk <- corpus[[1]]$content[1:10]
 test <- wc(buk)
-truth <- structure(list(chars = 110L, letters = 92L, whitespace = 12L, 
+truth <-
+structure(list(chars = 110L, letters = 92L, whitespace = 12L, 
     punctuation = 4L, digits = 2L, words = 16L, sentences = 2L, 
-    lines = 10L), .Names = c("chars", "letters", "whitespace", 
-"punctuation", "digits", "words", "sentences", "lines"))
+    lines = 10L, wordlens = c(0L, 2L, 3L, 1L, 1L, 0L, 5L, 0L, 
+    0L, 3L, 1L, 0L, 0L, 0L, 0L, 0L), senlens = c(1L, 0L, 0L, 
+    1L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 
+    0L, 0L), syllens = c(5L, 2L, 2L, 0L, 0L, 0L, 0L, 0L, 0L, 
+    0L)), .Names = c("chars", "letters", "whitespace", "punctuation", 
+"digits", "words", "sentences", "lines", "wordlens", "senlens", 
+"syllens"))
 
-stopifnot(all(sapply(2:length(test)-1, function(i) test[[i]] == truth[[i]])))
-
+stopifnot(identical(test, truth))
 
 
 
 
 buk <- corpus[[1]]$content[21:27]
 test <- wc(buk)
-truth <- structure(list(chars = 359L, letters = 281L, whitespace = 66L, 
+truth <-
+structure(list(chars = 359L, letters = 281L, whitespace = 66L, 
     punctuation = 12L, digits = 0L, words = 72L, sentences = 5L, 
     lines = 7L, wordlens = c(2L, 16L, 16L, 13L, 12L, 3L, 4L, 
-    4L, 0L, 1L, 1L, 0L, 0L, 0L, 0L, 0L)), .Names = c("chars", 
-"letters", "whitespace", "punctuation", "digits", "words", "sentences", 
-"lines", "wordlens"))
+    4L, 0L, 1L, 1L, 0L, 0L, 0L, 0L, 0L), senlens = c(1L, 1L, 
+    0L, 1L, 0L, 0L, 1L, 0L, 0L, 0L, 0L, 0L, 0L, 1L, 0L, 0L, 0L, 
+    0L, 0L, 0L), syllens = c(41L, 13L, 5L, 1L, 0L, 0L, 0L, 0L, 
+    0L, 0L)), .Names = c("chars", "letters", "whitespace", "punctuation", 
+"digits", "words", "sentences", "lines", "wordlens", "senlens", 
+"syllens"))
 
-stopifnot(all(sapply(2:length(test)-1, function(i) test[[i]] == truth[[i]])))
-
-
+stopifnot(identical(test, truth))
